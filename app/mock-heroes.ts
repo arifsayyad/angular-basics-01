687873da86857a893e0f28f9caade2f7ae00ff68
Arif Sayyad <arifmsayyad@gmail.com>
2016-10-23 22:29:01 -0500

    added dashboard with navigation

---
+++ app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{ id: 11, name: 'Mr. Nice' },
+{ id: 12, name: 'Narco' },
+{ id: 13, name: 'Arif' },
+{ id: 14, name: 'Hakkim' },
+{ id: 15, name: 'Honey' },
+{ id: 16, name: 'Amit' },
+{ id: 17, name: 'Abhay' },
+{ id: 18, name: 'Vinay' },
+{ id: 19, name: 'keerthi' },
+{ id: 20, name: 'Poonam' }
+];
